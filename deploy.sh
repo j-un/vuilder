b636b88ee0db7e9097af497b8a088d0b64385102
Jun TANAKA <tanaka.jun@cloudpayment.co.jp>
2018-09-24 01:13:45 +0900

    Update comments

--- deploy.sh
+++ deploy.sh
@@ -5,17 +5,17 @@ set -e
 base="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [[ $# -ne 1 ]]; then
-    echo "You must set a version number"
-    echo "./deploy.sh <vue-cli version>"
-    exit 1
+  echo "You must set a vuilder version number(same as vue-cli version)"
+  echo "./deploy.sh <vuilder version>"
+  exit 1
 fi
 
 version=$1
 
-echo "Building docker images for vuilder ${version}..."
+echo "Building docker image for vuilder ${version}..."
 docker build -f "${base}/Dockerfile" -t nolabo/vuilder:${version} --build-arg version=${version} .
 docker tag nolabo/vuilder:${version} nolabo/vuilder:latest
 
-echo "Uploading docker images for vuilder ${version}..."
+echo "Uploading docker image for vuilder ${version}..."
 docker push nolabo/vuilder:${version}
 docker push nolabo/vuilder:latest
